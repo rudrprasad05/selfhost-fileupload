4a2e61084fa0fdca9044fbab68baf9ffb3e9872a
Rudr <rudrprasad@yahoo.com>
2024-07-17 13:39:50 +1200

    refactor multer storage

--- backend/app.js
+++ backend/app.js
@@ -2,25 +2,10 @@ const express = require("express");
 const multer = require("multer");
 const path = require("path");
 const { checkAuthToken } = require("./auth.js");
+const { upload } = require("./upload.js");
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, `./uploads/${req.headers["bucket"]}/`);
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(
-      null,
-      file.fieldname + "-" + uniqueSuffix + "." + file.mimetype.split("/")[1]
-    );
-  },
-});
-const upload = multer({ storage });
-
-// Middleware to check auth token
 
 app.post("/api/upload", checkAuthToken, upload.single("file"), (req, res) => {
   res.status(200).json({ message: "Image Uploaded Successfully" });
---
+++ backend/upload.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, `./uploads/${req.headers["bucket"]}/`);
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(
+      null,
+      file.fieldname + "-" + uniqueSuffix + "." + file.mimetype.split("/")[1]
+    );
+  },
+});
+const upload = multer({ storage });
+
+module.exports = { upload };
